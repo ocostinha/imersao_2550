a0e828a30e5f704c0f9e60b80653f846fb6d8ae3
felipecosta <fcosta.paltrinieri@gmail.com>
2024-04-22 21:48:43 -0300

    Comit inicial - spring initialzr

---
+++ src/main/java/br/com/fiap/imersao_2550/Imersao2550Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.imersao_2550;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Imersao2550Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Imersao2550Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/imersao_2550/Imersao2550ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.imersao_2550;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Imersao2550ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
